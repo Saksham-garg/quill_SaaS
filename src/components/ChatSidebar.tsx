004f76e7fc0b7129673ff03b6c3f067dc8e5ba9b
Saksham-garg <sakshamgarg142@hotmail.com>
2024-08-07 23:09:06 +0530

    integrate delete chat dialog

--- src/components/ChatSidebar.tsx
+++ src/components/ChatSidebar.tsx
@@ -4,11 +4,28 @@ import React, { useState } from 'react'
 import { DrizzleChats } from '@/lib/db/schema'
 import Link from 'next/link'
 import { Button } from './ui/button'
-import { MessageCircle, PlusCircle } from 'lucide-react'
+import { MessageCircle, PlusCircle, Trash2 } from 'lucide-react'
 import { cn } from '@/lib/utils'
 import axios from 'axios'
 import toast from 'react-hot-toast'
 import SubscriptionButton from './ui/SubscriptionButton'
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogFooter,
+    DialogTrigger,
+    DialogClose
+  } from "./dialog"
+import { useMutation } from '@tanstack/react-query'
+import { db } from '@/lib/db'
+import { auth } from '@clerk/nextjs/server'
+import { eq } from 'drizzle-orm'
+import { chats } from "@/lib/db/schema";
+import { useRouter } from 'next/navigation'
 
 type Props = {
     chats:DrizzleChats[],
@@ -16,8 +33,45 @@ type Props = {
     isPro: boolean
 }
 
-const ChatSidebar = ({chats,chatsId,isPro}: Props) => {
 
+const ChatSidebar = ({chats:Chats,chatsId,isPro}: Props) => {
+    
+    const router = useRouter()
+    const { userId } = auth()
+    let firstChat;
+    if(userId){
+        firstChat = await db.select().from(chats).where(eq(chats.userId,userId))
+        if(firstChat){
+            firstChat = firstChat[0]
+        }
+    }
+    }
+    const { mutate } = useMutation({
+        mutationFn: async({chatId}:{chatId:number}) => {
+            try {
+                const response = await axios.delete(`/api/delete-chat/${chatId}`)
+                return response.data
+            } catch (error:any) {
+                toast.error(error.response)
+            }
+        }
+    })
+
+    const deleteChat = (chat:{chatId:number,firstChat:number,router:AppRouterInstance}) => {
+        mutate(chat,
+            {
+                onError:(err) => {
+                    console.log("err",err)
+                    toast.error(err.message)
+                },
+                onSuccess:() => {
+                    toast.success("Chats created successfully.")
+                    chat.router.push(`/chat/${chat.firstChat?.id}`)
+                }
+            }
+            )
+    }
+    
   return (
     <div className='w-full h-screen p-4 text-gray-200 bg-gray-900'>
         <Link href="/">
@@ -30,17 +84,41 @@ const ChatSidebar = ({chats,chatsId,isPro}: Props) => {
         <div className="flex max-h-screen overflow-scroll pb-20 flex-col gap-2 mt-4">
             {
                 chats.map((chat) => {
-                    return <Link  key={chat.id} href={`/chat/${chat.id}`}>
-                        <div className={cn("rounded-lg p-3 text-slate-300 flex items-center",{
-                            "bg-blue-800 text-white":chat.id == chatsId,
-                            "hover:text-white":chat.id !== chatsId
-                        })}>
-                            <MessageCircle className='mr-2' />
-                            <p className='w-full overflow-hidden text-sm truncate whitespace-nowrap text-ellipsis'>
-                                {chat.pdfName}
-                            </p>
-                        </div>
-                    </Link>
+                    return <div className={cn("rounded-lg p-3 text-slate-300 flex justify-between items-center gap-1",{
+                        "bg-blue-800 text-white":chat.id == chatsId,
+                        "hover:text-white":chat.id !== chatsId
+                    })}>
+                            <Link  key={chat.id} href={`/chat/${chat.id}`}>
+                                <div className={cn("rounded-lg text-slate-300 flex items-center")}>
+                                    <MessageCircle className='mr-2' />
+                                    <p className='w-full overflow-hidden text-sm truncate whitespace-nowrap text-ellipsis'>
+                                        {chat.pdfName}
+                                    </p>
+                                </div>
+                            </Link>
+                            <Dialog>
+                                <DialogTrigger>
+                                        <Trash2 className='cursor-pointer'/>
+                                </DialogTrigger>
+                                <DialogContent className="sm:max-w-[425px]">
+                                    <DialogHeader>
+                                    <DialogTitle>Are you absolutely sure?</DialogTitle>
+                                    <DialogDescription>
+                                       <p className='mt-3'>
+                                       This action cannot be undone. This will permanently delete <span className='font-bold'>{chat.pdfName} </span> PDF data, chats and remove this data from our servers.
+                                       </p>
+                                    </DialogDescription>
+                                    </DialogHeader>
+                                       <DialogFooter>
+                                        <DialogClose asChild>
+                                            <Button type="button" variant="secondary">Cancel</Button>
+                                        </DialogClose>
+                                            <Button type="submit" onSubmit={() => deleteChat(chat.id)}>Delete</Button>
+                                        </DialogFooter>
+                                </DialogContent>
+                            </Dialog>
+                    </div>
+                    
                 })  
             }
         </div>
